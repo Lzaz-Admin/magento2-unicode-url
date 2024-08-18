634b9c60554839c08b0676d42db2d30ea0f0e603
Mohammad Almutairi <155480548+Lzaz-Admin@users.noreply.github.com>
2024-08-18 20:24:03 +0300

    Update Category.php

--- Plugin/Model/Category.php
+++ Plugin/Model/Category.php
@@ -4,6 +4,9 @@ namespace SR\UnicodeUrl\Plugin\Model;
 
 class Category
 {
+    // Declare the property explicitly
+    private \Magento\Framework\Filter\FilterManager $filter;
+
     public function __construct(
         \Magento\Framework\Filter\FilterManager $filter
     ) {
